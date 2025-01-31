edac8c2f8edcab5c23c0082243e33ab015ee33c9
parker <pliabraaten@gmail.com>
2025-01-30 19:04:14 -0800

    Configures REST API for read-only by disabling POST, PUT, DELETE methods in MyDataRestConfig

--- 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/entity/Product.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/entity/Product.java
@@ -2,8 +2,6 @@ package com.PL.ecommerce.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
