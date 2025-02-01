188fe925ad17db9712d37a4bbcb9fcd92eec6813
parker <pliabraaten@gmail.com>
2025-01-31 20:42:17 -0800

    Integrates service with Angular component

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  // REST API
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  // Injects HttpClient  
+  constructor(private httpClient: HttpClient) { }
+
+  // Maps JSON data from Spring Data REST to product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+
+// Unwraps the JSON from Srping Data REST _embedded entry
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
