4a61e04d92fcad33f403de1a984834a8c6a4b555
parker <pliabraaten@gmail.com>
2025-01-31 20:55:35 -0800

    Allows CrossOrigin support so front-end (localhost:4200) makes call to back-end (localhost:8080/api/products)

--- 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,10 @@ package com.PL.ecommerce.dao;
 import com.PL.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")  // Accepts calls from web browser scripts for this origin
+                                        // Origin is protocol + hostname + port
 // Sets relationship and path; name of JSON entry = productCategory ; path: /product-category
     // Custom: otherwise default just adds s to path at end of entity name
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
--- 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductRepository.java
@@ -2,8 +2,11 @@ package com.PL.ecommerce.dao;
 
 import com.PL.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Repository interface: defines a set of standard data access methods (CRUD operations, pagination, sorting, etc
+@CrossOrigin("http://localhost:4200")  // Accepts calls from web browser scripts for this origin
+                                        // Origin is protocol + hostname + port
 public interface ProductRepository extends JpaRepository<Product, Long> {  // Entity=Product, PrimaryKeyType=Long
 
 
