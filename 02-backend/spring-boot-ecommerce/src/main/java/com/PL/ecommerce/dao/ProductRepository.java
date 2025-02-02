f77a069b849b9bc32b9837b47f23cd9c6785d1b6
parker <pliabraaten@gmail.com>
2025-02-02 15:38:55 -0800

    Updates Angular product.service.ts to call the new search URL on Spring Boot app

--- 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // Repository interface: defines a set of standard data access methods (CRUD operations, pagination, sorting, etc
 @CrossOrigin("http://localhost:4200")  // Accepts calls from web browser scripts for this origin
@@ -14,7 +15,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {  // En
 
     // Add new query method that matches category id by the parameter id value
         // Behind the scenes, Spring will execute a query like this: SELECT * FROM product WHERE category_id=?
-    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
     // Spring Data REST automatically exposes endpoint  http://localhost:8080/api/products/search/findByCategoryId?id=2
 
 }
