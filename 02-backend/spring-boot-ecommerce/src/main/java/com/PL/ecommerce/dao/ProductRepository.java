9806498e5499a50b161fe0f9769fa967fb34e804
parker <pliabraaten@gmail.com>
2025-02-02 15:15:23 -0800

    Updates Spring Boot - REST Repository with new findByCategoryId method

--- 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/PL/ecommerce/dao/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.PL.ecommerce.dao;
 
 import com.PL.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Repository interface: defines a set of standard data access methods (CRUD operations, pagination, sorting, etc
@@ -9,5 +12,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
                                         // Origin is protocol + hostname + port
 public interface ProductRepository extends JpaRepository<Product, Long> {  // Entity=Product, PrimaryKeyType=Long
 
+    // Add new query method that matches category id by the parameter id value
+        // Behind the scenes, Spring will execute a query like this: SELECT * FROM product WHERE category_id=?
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    // Spring Data REST automatically exposes endpoint  http://localhost:8080/api/products/search/findByCategoryId?id=2
 
 }
