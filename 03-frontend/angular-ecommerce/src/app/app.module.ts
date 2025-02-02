26a3c479105b68ef981a162d103f16b4422fd5f6
parker <pliabraaten@gmail.com>
2025-02-02 14:39:00 -0800

    Defines Angular routes and configures Router

--- 03-frontend/angular-ecommerce/src/app/app.module.ts
+++ 03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -6,12 +6,26 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';  // Import for the product service
 import { ProductService } from './services/product.service';
 
+import { Routes, RouterModule } from '@angular/router';
+
+// Add routes for category links
+const routes: Routes = [
+  // When path matches, create new instance of component
+    // Checks paths in order listed
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},  // If not path given, redirect back; pathMatch on full path not just prefix
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},  // Wildcard path, any path that doesn't match the above path options
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule  // Import for the product service
   ],
